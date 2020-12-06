e6d5e8f2f5a4f41bedd4a442fce30c684d4730e7
Jmnoland <jody-noreply@git.com>
2020-12-06 21:52:40 +0200

    Added methods to encryptionService

--- FolderEncryption/Interfaces/IEncryptionService.cs
+++ FolderEncryption/Interfaces/IEncryptionService.cs
@@ -8,6 +8,9 @@ namespace FolderEncryption.Interfaces
 {
     public interface IEncryptionService
     {
-
+        void CreateNewKey(string containerName);
+        void CreatePublicKeyFromXML(string containerName, string xmlKeyInfo);
+        byte[] EncryptFile(string containerName, byte[] data);
+        byte[] DecryptFile(string containerName, byte[] data);
     }
 }
--- FolderEncryption/Services/EncryptionService.cs
+++ FolderEncryption/Services/EncryptionService.cs
@@ -1,14 +1,77 @@
 ﻿using FolderEncryption.Interfaces;
+using FolderEncryption.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace FolderEncryption.Services
 {
     public class EncryptionService : IEncryptionService
     {
+        private IFileEncryptionRepository _fileEncryptionRepository;
+        private Dictionary<string, RSACryptoServiceProvider> _publicKeys;
+        private RSAEncryptionPadding _padding;
+        public EncryptionService(IFileEncryptionRepository fileEncryptionRepository)
+        {
+            _fileEncryptionRepository = fileEncryptionRepository;
+            _padding = RSAEncryptionPadding.OaepSHA512;
+        }
 
+        public void CreateNewKey(string containerName)
+        {
+            var param = new CspParameters
+            {
+                KeyContainerName = containerName
+            };
+            using(var rsa = new RSACryptoServiceProvider(param))
+            {
+                _fileEncryptionRepository.AddEncryptionKey(new EncryptionKey
+                {
+                    CreateDate = DateTime.Now.ToString(),
+                    PublicKey = rsa.ToXmlString(false)
+                });
+            }
+        }
+
+        public void CreatePublicKeyFromXML(string containerName, string xmlKeyInfo)
+        {
+            var publicKey = new RSACryptoServiceProvider();
+            publicKey.FromXmlString(xmlKeyInfo);
+            _publicKeys.Add(containerName, publicKey);
+        }
+
+        public byte[] EncryptFile(string containerName, byte[] data)
+        {
+            RSACryptoServiceProvider publicKey = null;
+            _publicKeys.TryGetValue(containerName, out publicKey);
+            if (publicKey == null) throw new Exception("Encryption failed: Public key not found");
+            return publicKey.Encrypt(data, _padding);
+        }
+
+        public byte[] DecryptFile(string containerName, byte[] data)
+        {
+            var param = new CspParameters
+            {
+                KeyContainerName = containerName
+            };
+            using (var rsaKey = new RSACryptoServiceProvider(param))
+            {
+                RSACryptoServiceProvider publicKey = null;
+                _publicKeys.TryGetValue(containerName, out publicKey);
+
+                if (publicKey == null)
+                {
+                    rsaKey.PersistKeyInCsp = false;
+                    throw new Exception("Decryption failed: Public key not found");
+                }
+                if (publicKey.ToXmlString(false) != rsaKey.ToXmlString(false))
+                {
+                    rsaKey.PersistKeyInCsp = false;
+                    throw new Exception("Decryption failed: Doesn't match existing key");
+                }
+
+                return rsaKey.Decrypt(data, _padding);
+            }
+        }
     }
 }
